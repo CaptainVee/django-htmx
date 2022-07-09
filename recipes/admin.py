cfdcf6ea10039ba21fb2a30514de2f6cf6577941
CaptainVee <captainvee3@gmail.com>
2022-07-09 22:25:51 +0100

    more work on recipe

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Recipe, RecipeIngredient
+
+
+# class RecipeIngredientInline(admin.TabularInline):
+#     model = RecipeIngredient
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['user', 'name']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(RecipeIngredient)
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,26 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 
 class Recipe(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
